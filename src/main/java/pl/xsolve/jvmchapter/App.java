ac44cc9565b43d1a73302993c86f30099260eae4
Maciej Papież <maciej.papiez@xsolve.pl>
2017-09-20 09:46:13 +0200

    Demo app

---
+++ src/main/java/pl/xsolve/jvmchapter/App.java
@@ -0,0 +1,8 @@
+package pl.xsolve.jvmchapter;
+
+public class App {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/main/java/pl/xsolve/jvmchapter/Bar.java
@@ -0,0 +1,4 @@
+package pl.xsolve.jvmchapter;
+
+public class Bar {
+}
